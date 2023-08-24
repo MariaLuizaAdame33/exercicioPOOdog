1bc7578c59aa79bb7f2293e3fadc824dbc911a56
MariaLuizaAdame33 <maluzinha.adame@gamil.com>
2023-08-24 10:21:31 -0300

    pessoa

---
+++ pessoa.js
@@ -0,0 +1,24 @@
+"use strict";
+class Pessoa {
+    constructor(nome, idade, comidas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.comidas = comidas;
+    }
+    cumprimentar() {
+        let msgHabilitação = " ja posso tirar minha CNH";
+        if (this.idade < 18) {
+            msgHabilitação = " Não posso tirar minha CNH";
+        }
+        return "Ola meu nome é " + this.nome + " tenho " + this.idade + "anos" + msgHabilitação + ".";
+    }
+    comidasFavoritas() {
+        console.log("Minha(s) comida(s) favoritas(s)");
+        for (let i = 0; i < this.comidas.length; i++) {
+            console.log(this.comidas[i]);
+        }
+    }
+}
+let Pessoa1 = new Pessoa("Maria", 16, ["bolo", "lasanha"]);
+console.log(Pessoa1.cumprimentar());
+console.log(Pessoa1.comidasFavoritas());
