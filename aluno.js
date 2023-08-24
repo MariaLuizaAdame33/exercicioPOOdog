5026b3ff9b77f88b4ae9779bac1a6324db2d38b0
MariaLuizaAdame33 <maluzinha.adame@gamil.com>
2023-08-24 15:45:25 -0300

    alunos media

---
+++ aluno.js
@@ -0,0 +1,25 @@
+"use strict";
+class Estudante {
+    constructor(nome, idade, nota) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nota = nota;
+    }
+    apresentacao() {
+        return "Aluno: " + this.nome + " Idade: " + this.idade + " anos  Notas:" + this.nota;
+    }
+    Calculo() {
+        let soma = 0;
+        for (let i = 0; i < this.nota.length; i++) {
+            soma += this.nota[i];
+        }
+        const media = soma / this.nota.length;
+        if (media < 7) {
+            return this.nome + ", sua média é " + media + "  e você precisa estudar mais para melhorar seu desempenho. ";
+        }
+        return "Parabéns, " + this.nome + " Sua média é " + media + " e você está aprovado(a).";
+    }
+}
+let Aluno1 = new Estudante("Maria", 20, [8, 7.5, 9.2, 6.8, 9.5]);
+console.log(Aluno1.apresentacao());
+console.log(Aluno1.Calculo());
